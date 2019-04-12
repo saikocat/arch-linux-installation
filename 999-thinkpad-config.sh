71f89a152243e473cf2f3d8cd1665391f88e8860
Duc Hoa Nguyen <hoameomu@gmail.com>
2019-04-12 19:19:43 +0800

    Fix thinkpad script

--- 999-thinkpad-config.sh
+++ 999-thinkpad-config.sh
@@ -6,15 +6,15 @@ trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 exec 1> >(tee "stdout.999.log")
 exec 2> >(tee "stderr.999.log")
 
-pacstrap /mnt thinkpad_acpi acpi_call powertop tlp tlp-rdw
+pacstrap /mnt acpi_call powertop tlp tlp-rdw
 install -Dm0644 system-config/mod-thinkpad-acpi.conf /mnt/etc/modprobe.d/thinkpad-acpi.conf
 
-arch-chroot /mnt systemctl NetworkManager-dispatcher.service
+arch-chroot /mnt systemctl enable NetworkManager-dispatcher.service
 arch-chroot /mnt systemctl enable tlp.service
 arch-chroot /mnt systemctl enable tlp-sleep.service
 arch-chroot /mnt systemctl disable systemd-rfkill.service
 arch-chroot /mnt systemctl mask systemd-rfkill.service
 arch-chroot /mnt systemctl mask systemd-rfkill.socket
 
-echo "START_CHARGE_THRESH_BAT0=75" >> /etc/default/tlp
-echo "STOP_CHARGE_THRESH_BAT0=80" >> /etc/default/tlp
+sed -i '/START_CHARGE_THRESH_BAT0=/s/^#//g' /mnt/etc/default/tlp
+sed -i '/STOP_CHARGE_THRESH_BAT0=/s/^#//g'  /mnt/etc/default/tlp
