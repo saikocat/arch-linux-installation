754c04644cba564aa3954f639cbed087d6becfca
Hoa <hoameomu@gmail.com>
2019-04-13 15:28:24 +0800

    Add Desktop apps

--- 003-desktop.sh
+++ 003-desktop.sh
@@ -9,8 +9,43 @@ exec 2> >(tee "stderr.3.log")
 
 USER=hoa
 
-pacstrap -S xorg-server plasma sddm konsole yakuake dolphin fish chromium firefox okular
-arch-chroot /mnt systemctl enable sddm.service
+systemctl enable --now systemd-timesyncd.service
+timedatectl set-ntp true
+timedatectl show-timesync --all
+timedatectl status
+timedatectl timesync-status
+
+pacman -S --noconfirm fish
+useradd -mU -s /usr/bin/fish -G wheel,input,sys "$USER"
+
+pacman -S --noconfirm openssh sshfs
+systemctl enable sshd.socket sshd@.service
+
+systemctl enable bluetooth.service
+
+pacman -S --noconfirm unrar unzip tmux tree fwupd gnu-netcat p7zip stow nfs-utils crda fzf
+
+pacman -S --noconfirm xorg-server xorg-xrandr
+pacman -S --noconfirm ttf-liberation powerline-fonts ttf-croscore ttf-carlito ttf-caladea ttf-fira-code otf-hasklig adobe-source-code-pro-fonts otf-ipafont lcdfilter
+
+pacman -S --noconfirm cups foomatic-db-engine foomatic-db foomatic-db-ppds foomatic-db-nonfree-ppds foomatic-db-gutenprint-ppds
+systemctl enable org.cups.cupsd.service
+
+pacman -S --noconfirm plasma-desktop plasma-nm plasma-workspace sddm \
+    phonon-qt5-gstreamer phonon-qt5-vlc kinfocenter kscreen kscreenlocker \
+    kmenuedit kwalletmanager ksysguard kwin breeze breeze-gtk kwrite \
+    kde-cli-tools kde-gtk-config kdecoration kdeplasma-addons kgamma5 khotkey \
+    latte-dock powerdevil yakuake \
+    konsole yakuake dolphin ark spectacle okular gwenview adapta-gtk-theme \
+    chromium firefox mcomix keepassx kdeconnect \
+    pulseaudio pulseaudio-bluetooth pavucontrol-qt \
+    alsa gstreamer gst-plugins-good gst-plugins-ugly gst-libav \
+    gstreamer-vaapi libva-intel-driver mpv  \
+    tranmission-qt remmina
+systemctl enable sddm.service
+
+pacman -S --noconfirm libreoffice-still hunspell-en_GB ibus-unikey
+
 # arch-chroot /mnt useradd -mU -s /usr/bin/fish -G wheel,uucp,video,audio,storage,games,input,sys "$USER"
 # arch-chroot /mnt chsh -s /usr/bin/fish
 
@@ -19,24 +54,8 @@ arch-chroot /mnt systemctl enable sddm.service
 # makepkg -si
 # Color pacman
 
-# ttf-liberation powerline-fonts ttf-croscore ttf-carlito ttf-caladea ttf-fira-code otf-hasklig adobe-source-code-pro-fonts otf-ipafont lcdfilter
+# yay -S nvme-cli nerd-fonts-source-code-pro otf-san-francisco otf-sfmono ttf-public-sans pandoc-bin
+# yay -c -Sc
+# libinput-gestures  mill yay-bin stack-static
 
-# openssh nfs-utils hddtemp tmux
-# systemctl enable sshd.socket
-# calibre libreoffice-still hunspell pulseaudio alsa keepassxc
-
-# sudo systemctl start bluetooth.service
-
-# audio & codec
-# phonon-qt5-gstreamer phonon-qt5-vlc gst-plugins-good gst-plugins-ugly gst-libav
-# libva-intel-driver gstreamer-vaapi
-
-# stow tree ark p7zip spectacle mcomix okular gwenview mpv curl wget
-
-# cups foomatic-db-engine foomatic-db foomatic-db-ppds foomatic-db-nonfree-ppds foomatic-db-gutenprint-ppds
-# sudo systemctl enable org.cups.cupsd.service
-
-# adapta-gtk-theme
-
-
-# qemu ovmf
+# yay -S telegram-desktop
---
+++ 050-dev.sh
@@ -0,0 +1,12 @@
+nnn tig
+qemu ovmf
+go
+docker-compose
+texinfo
+texlive-bin
+texlive-core
+texlive-fontsextra
+texlive-formatsextra
+texlive-latexextra
+python-virtualenvwrapper
+jre8-openjdk scala
